d4169d9aeb1cb3f559f8602c848b6f403422d712
Plai <168644089+plai-os@users.noreply.github.com>
2026-07-07 07:47:30 +0100

    Fit Treasure Trail controls in popup

--- plai.js
+++ plai.js
@@ -3824,7 +3824,7 @@ function closeBubblegumStampedeModal() {
   document.body.classList.remove("plai-bubblegum-game-open");
 }
 
-const PROTOTYPE_GAME_RELEASE = "20260707-treasure-graphics";
+const PROTOTYPE_GAME_RELEASE = "20260707-treasure-fit";
 const PROTOTYPE_GAME_CONFIG = {
   animal: {
     title: "Animal Stampede",
